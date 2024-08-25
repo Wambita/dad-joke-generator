5bbe4f94e85bf372fcd76d12c9937e4d7aa8cff7
shfana <wambitafana@gmail.com>
2024-08-25 04:06:10 +0300

    Feat: add handlers for errors and wrong method

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -30,12 +30,36 @@ func Homehandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, data)
 }
 
-// // api handler
-// func jokeHandler(w http.ResponseWriter, r *http.Request) {
-// 	joke, err := logic.GetJoke()
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-// 	fmt.Fprint(w, joke)
-// }
+// error data struct
+type ErrorData struct {
+	StatusCode int
+	Message    string
+}
+
+// error handler
+func ErrorHandler(w http.ResponseWriter, r *http.Request, statusCode int, message string) {
+	w.WriteHeader(statusCode)
+
+	// prepare  the error data to pass to  pass the template
+	data := ErrorData{
+		StatusCode: statusCode,
+		Message:    message,
+	}
+	// parse and execute the error template
+	tmplPath := filepath.Join("templates", "error.html")
+	tmpl := template.Must(template.ParseFiles(tmplPath))
+	tmpl.Execute(w, data)
+}
+
+// method handler
+func MethodHandler(h http.HandlerFunc, allowedMethod string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != allowedMethod {
+			ErrorHandler(w, r, http.StatusMethodNotAllowed, "405 - Method Not Allowed")
+			return
+		}
+		h(w, r)
+	}
+}
+
+//
