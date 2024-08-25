2d32830bb4427ccc38bb39e06921b060e3bfa776
shfana <wambitafana@gmail.com>
2024-08-25 04:08:08 +0300

    Feat: add handleFunc for error page and wrong methods

--- main.go
+++ main.go
@@ -8,10 +8,18 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handler.Homehandler)
-	// http.HandleFunc("/joke", jokeHandler)
+	// http.HandleFunc("/", handler.MethodHandler(handler.Homehandler, "GET"))
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static")))) // http.HandleFunc("/joke", jokeHandler)
+
+	// catch all route for unhandles paths
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" && r.URL.Path != "/static/" {
+			handler.ErrorHandler(w, r, http.StatusNotFound, "404 -  Not Found")
+			return
+		}
+		handler.Homehandler(w, r)
+	})
 
 	fmt.Println("Server is running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
